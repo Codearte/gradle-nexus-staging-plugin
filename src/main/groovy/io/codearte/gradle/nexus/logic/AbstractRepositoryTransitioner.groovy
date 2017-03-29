18a3bbc24828de5dd6149e1447a60009c435643a
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2017-03-29 12:34:43 +0200

    [#43][#39] Dependencies upgrade
    Gradle 3.3, plugins and dependencies. Added uptodate plugin.

--- src/main/groovy/io/codearte/gradle/nexus/logic/AbstractRepositoryTransitioner.groovy
+++ src/main/groovy/io/codearte/gradle/nexus/logic/AbstractRepositoryTransitioner.groovy
@@ -12,6 +12,7 @@ abstract class AbstractRepositoryTransitioner extends BaseOperationExecutor impl
                         stagedRepositoryId: repositoryId,
                         description: 'Automatically released/promoted with gradle-nexus-staging-plugin!',
                         targetRepositoryId: stagingProfileId
-                ]]
+                ]] as Map<String, Map>  //coercion required to prevent: Incompatible generic argument types. Cannot assign java.util.LinkedHashMap
+                                        // <java.lang.String, java.util.LinkedHashMap> to: java.util.Map <String, Map> in Groovy 2.4.10
     }
 }
