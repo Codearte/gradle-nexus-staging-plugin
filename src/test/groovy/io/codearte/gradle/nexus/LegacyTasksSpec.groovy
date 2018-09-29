424cb396002d7926bec44fc34dd12b3044452c5b
Marcin Zajączkowski <szpak@users.sourceforge.net>
2018-09-29 23:58:02 +0200

    Trigger project evaluation directly
    With using internal Gradle API.

--- src/test/groovy/io/codearte/gradle/nexus/LegacyTasksSpec.groovy
+++ src/test/groovy/io/codearte/gradle/nexus/LegacyTasksSpec.groovy
@@ -2,6 +2,7 @@ package io.codearte.gradle.nexus
 
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
@@ -33,7 +34,8 @@ class LegacyTasksSpec extends Specification {
     }
 
     private void triggerEvaluate() {
-        getJustOneTaskByNameOrFail('tasks')
+        //Not available in public API, alternatively 'getJustOneTaskByNameOrFail("tasks")' could be used
+        ((ProjectInternal) project).evaluate()
     }
 
     private Task getJustOneTaskByNameOrFail(String taskName) {
